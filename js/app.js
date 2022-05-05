1bd052974108c3ea78a2a05d00609fcd9d01d5d4
BastienGavioli <bastien.gavioli@etu.umontpellier.fr>
2022-05-05 19:54:59 +0200

    Adding a Texture

--- js/app.js
+++ js/app.js
@@ -9,7 +9,7 @@ document.body.appendChild(renderer.domElement);
 const sphere = new THREE.Mesh(
     new THREE.SphereGeometry(5, 50, 50),
     new THREE.MeshBasicMaterial({
-        color: 0xff0000
+        map: new THREE.TextureLoader().load("./resources/img/globe.jpg")
     })
 );
 
